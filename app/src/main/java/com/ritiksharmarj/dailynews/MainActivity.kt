1bfbe576b6152ba9ce217706d3ea02d7197a15f6
ritiksharmarj <bloggeritik@gmail.com>
2022-12-26 16:02:53 +0530

    Endless scroll added.

--- app/src/main/java/com/ritiksharmarj/dailynews/MainActivity.kt
+++ app/src/main/java/com/ritiksharmarj/dailynews/MainActivity.kt
@@ -2,11 +2,12 @@ package com.ritiksharmarj.dailynews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ritiksharmarj.dailynews.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,11 +17,54 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     lateinit var adapter: NewsAdapter
+    lateinit var layoutManager: LinearLayoutManager
+    private var articles = mutableListOf<Article>()
+    private var isScrolling = false
+    private var currentItems = 0
+    private var totalItems = 0
+    private var scrollOutItems = 0
+    var totalResults = -1
+    var pageNumber = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapter = NewsAdapter(this, articles)
+        binding.newsList.adapter = adapter
+        layoutManager = LinearLayoutManager(this)
+        binding.newsList.layoutManager = layoutManager
+
+        binding.newsList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    isScrolling = true
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                currentItems = layoutManager.childCount
+                totalItems = layoutManager.itemCount
+                scrollOutItems = layoutManager.findFirstVisibleItemPosition()
+
+                if (isScrolling) {
+                    if (totalResults > totalItems && scrollOutItems >= totalItems - 5) {
+                        isScrolling = false
+                        pageNumber++
+                        getNews()
+                    }
+                }
+
+                /*if (isScrolling && (currentItems + scrollOutItems == totalItems)) {
+                    isScrolling = false
+                    pageNumber++
+                    getNews()
+                }*/
+            }
+        })
 
         getNews()
 
@@ -43,19 +87,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getNews() {
-        val news = NewsService.newsInstance.getHeadlines("in", 1)
+        val news = NewsService.newsInstance.getHeadlines("in", pageNumber)
         news.enqueue(object : Callback<News> {
             override fun onResponse(call: Call<News>, response: Response<News>) {
                 val news = response.body()
                 if (news != null) {
-                    adapter = NewsAdapter(this@MainActivity, news.articles)
-                    binding.newsList.adapter = adapter
-                    binding.newsList.layoutManager = LinearLayoutManager(this@MainActivity)
+                    totalResults = news.totalResults
+                    articles.addAll(news.articles)
+                    adapter.notifyDataSetChanged()
                 }
             }
 
             override fun onFailure(call: Call<News>, t: Throwable) {
-                Log.d("NEWS", "Error in fetching news", t)
+                Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
             }
         })
     }
